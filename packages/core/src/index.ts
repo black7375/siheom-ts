a51d36b1bd37fee3fa3f4abe1e547341d8b4a525
alstjr7375 <alstjr7375@daum.net>
2026-08-24 00:00:00 +0900

    feat: connect browser consumer to core exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,12 +4,14 @@ export { defaultMessageMap, formatFailureReport, resolveMessageMap } from "./mes
 export type { MessageMap } from "./messages.js";
 export { extendSiheom, overrideSiheom } from "./factory.js";
 export type { SiheomBindings, SiheomFactoryRegistries } from "./factory.js";
-export { query, getElement } from "./query.js";
+export { query, getElement, locatorLog } from "./query.js";
 export { actions, defaultActions, createDefaultActions } from "./action.js";
 export type { DefaultActions } from "./action.js";
 export { assertions, defaultAssertions, createDefaultAssertions } from "./assert.js";
 export type { DefaultAssertions } from "./assert.js";
 export type { A11ySnapshotOptions } from "./getA11ySnapshot.js";
+export { getA11ySnapshot } from "./getA11ySnapshot.js";
+export { tableToMarkdown } from "./tableToMarkdown.js";
 export { effect, defaultEffects } from "./effect.js";
 export { withFakeTimers } from "./withFakeTimers.js";
 export {
